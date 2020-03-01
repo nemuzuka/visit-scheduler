94cf0a8dee181135fe05c005d9f60d58cd4630d4
Kazumune Katagiri <kkaz123@gmail.com>
2020-03-01 10:39:20 +0900

    スケジュール Top 画面実装

---
+++ front/src/components/schedule/ScheduleItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="column is-3">
+    <div class="card card-area" style="cursor: pointer;" @click.stop="moveScheduleDetail">
+      <div class="card-content">
+        <p class="title">
+          {{schedule.target_year_and_month}} 分
+        </p>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'schedule-item',
+    props: ["schedule"],
+    methods: {
+      moveScheduleDetail() {
+        const self = this
+        self.$router.push('/detail-schedule/' + self.schedule.schedule_code)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .card-content p.title {
+    margin-top: 10px;
+  }
+
+  div.card-area {
+    border: solid #363636 2px;
+  }
+
+  div.card-area:hover {
+    background: gainsboro;
+    border: dashed #363636 2px;
+  }
+</style>
